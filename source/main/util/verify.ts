fbc40a903965609eb84337ff65cfdddbf742ed9a
Emanuel Palm <emanuel.palm@ltu.se>
2017-10-16 15:18:20 +0200

    Add source/main/util/verify.ts docs

--- source/main/util/verify.ts
+++ source/main/util/verify.ts
@@ -1,3 +1,9 @@
+/**
+ * Verifies that given value is an array, undefined or null.
+ *
+ * @param value Value to verify.
+ * @param message Verification failure message.
+ */
 export function isArrayOrNothing(value: any, message?: string): any[] {
     if (value === undefined || value === null || Array.isArray(value)) {
         return value;
@@ -5,6 +11,12 @@ export function isArrayOrNothing(value: any, message?: string): any[] {
     throw new TypeError("Not array or nothing: " + value);
 }
 
+/**
+ * Verifies that given value is an object.
+ *
+ * @param value Value to verify.
+ * @param message Verification failure message.
+ */
 export function isObject(value: any, message?: string): object {
     if (typeof value === "object") {
         return value;
@@ -12,6 +24,12 @@ export function isObject(value: any, message?: string): object {
     throw new TypeError(message || "Not an object: " + value);
 }
 
+/**
+ * Verifies that given value is an object, undefined or null.
+ *
+ * @param value Value to verify.
+ * @param message Verification failure message.
+ */
 export function isObjectOrNothing(value: any, message?: string): object {
     if (value === undefined || value === null || typeof value === "object") {
         return value;
@@ -19,6 +37,12 @@ export function isObjectOrNothing(value: any, message?: string): object {
     throw new TypeError(message || "Not object or nothing: " + value);
 }
 
+/**
+ * Verifies that given value is a string.
+ *
+ * @param value Value to verify.
+ * @param message Verification failure message.
+ */
 export function isString(value: any, message?: string): string {
     if (typeof value === "string") {
         return value;
