64139abdb250e180a918e830e6cf8aba5a77db9b
Emanuel Palm <emanuel.palm@ltu.se>
2017-10-17 16:45:35 +0200

    Continue work on main/ConfigurationSystem.ts

--- source/main/ConfigurationSystem.ts
+++ source/main/ConfigurationSystem.ts
@@ -5,8 +5,6 @@ import { ConfigurationStore } from "./ConfigurationStore";
 import * as db from "./db";
 import * as io from "./io";
 
-type WritableEntry = { path: string, value: apes.Writable };
-
 /**
  * Represents the Arrowhead Configuration System.
  */
@@ -22,12 +20,39 @@ export class ConfigurationSystem {
     public constructor(directory: db.Directory) {
         this.serviceManagement = new class implements ConfigurationManagement {
             addDocuments(documents: acml.Document[]): Promise<acml.Report[]> {
-                throw new Error("Method not implemented.");
+                const map = new Map<string, acml.Document[]>();
+                for (const document of documents) {
+                    const path = normalizePath(document.template);
+                    map.set(path, (map.get(path) || []).concat(document));
+                }
+                return this
+                    .listTemplates(Array.from(map.keys()))
+                    .then(templates => templates.reduce((reports, template) => {
+                        const documents = map.get(template.name) || [];
+                        for (let document; (document = documents.pop());) {
+                            reports.push(document);
+                        }
+                        return reports;
+                    }, new Array<acml.Report>()))
+                    .then(reports => {
+                        for (const documents of map.values()) {
+                            for (const document of documents) {
+                                reports.push(new acml.Report(
+                                    document.name,
+                                    document.template,
+                                    [{
+                                        path: ".",
+                                        condition: "template != undefined",
+                                    }]
+                                ));
+                            }
+                        }
+                        return reports;
+                    });
             }
 
             listDocuments(names: string[]): Promise<acml.Document[]> {
-                return list(".d", names)
-                    .then(entries => entries.map(acml.Document.read));
+                return list(".d", names, acml.Document.read);
             }
 
             removeDocuments(names: string[]): Promise<void> {
@@ -42,8 +67,7 @@ export class ConfigurationSystem {
             }
 
             listRules(names: string[]): Promise<acml.Rule[]> {
-                return list(".r", names)
-                    .then(entries => entries.map(acml.Rule.read));
+                return list(".r", names, acml.Rule.read);
             }
 
             removeRules(names: string[]): Promise<void> {
@@ -58,8 +82,7 @@ export class ConfigurationSystem {
             }
 
             listTemplates(names: string[]): Promise<acml.Template[]> {
-                return list(".t", names)
-                    .then(entries => entries.map(acml.Template.read));
+                return list(".t", names, acml.Template.read);
             }
 
             removeTemplates(names: string[]): Promise<void> {
@@ -75,33 +98,53 @@ export class ConfigurationSystem {
             }
         };
 
-        function add(prefix: string, entries: WritableEntry[]): Promise<void> {
+        function add(
+            prefix: string,
+            entries: { path: string, value: apes.Writable }[]
+        ): Promise<void> {
             const sink = new io.WritableBuffer(128);
             const writer = new apes.WriterJSON(sink);
             return directory.add(entries.map(entry => {
                 entry.value.write(writer);
                 return {
-                    path: addPrefix(prefix, entry.path),
+                    path: prefixPath(prefix, entry.path),
                     value: sink.pop(),
                 };
             }));
         }
 
-        function list(prefix: string, paths: string[]): Promise<object[]> {
+        function list<T>(
+            prefix: string,
+            paths: string[],
+            read: (value: object) => T
+        ): Promise<T[]> {
             return directory
-                .list(paths.map(path => addPrefix(prefix, path)))
-                .then(entries => entries.map(entry => {
-                    return JSON.parse(entry.value.toString());
+                .list(paths.map(path => prefixPath(prefix, path)))
+                .then(entries => new Promise<T[]>((resolve, reject) => {
+                    try {
+                        resolve(entries.map(entry => {
+                            return read(JSON.parse(entry.value.toString()));
+                        }));
+                    } catch (exception) {
+                        reject(exception);
+                    }
                 }));
         }
 
-        function remove(prefix: string, paths: string[]): Promise<void> {
-            return directory
-                .remove(paths.map(path => addPrefix(prefix, path)));
+        function remove(
+            prefix: string,
+            paths: string[]
+        ): Promise<void> {
+            paths = paths.map(path => prefixPath(prefix, path));
+            return directory.remove(paths);
         }
 
-        function addPrefix(prefix: string, path: string): string {
-            return prefix + (path.startsWith(".") ? "" : ".") + path;
+        function normalizePath(path: string): string {
+            return (path.startsWith(".") ? "" : ".") + path;
+        }
+
+        function prefixPath(prefix: string, path: string): string {
+            return prefix + normalizePath(path);
         }
     }
 
