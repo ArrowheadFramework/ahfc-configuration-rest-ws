cdde9c9591bd4d00bec5ae3824ccadab815cfced
Emanuel Palm <emanuel.palm@ltu.se>
2017-10-27 15:24:44 +0200

    Continue work on source/main/main.ts

---
+++ source/main/Settings.ts
@@ -0,0 +1,80 @@
+import * as ahfc from "ahfc-client";
+import * as crypto from "crypto";
+import * as fs from "fs";
+import * as process from "process";
+
+/**
+ * Holds application-wide settings.
+ */
+export class Settings {
+    private static appDataPath: string;
+
+    /**
+     * Creates new application settings object.
+     * 
+     * @param dnssd DNS-SD related options.
+     * @param databasePath Path to database folder.
+     * @param endpoint Domain name at which this service is available, if known.
+     * @param port Port number at which this service is available.
+     * @param instanceName Service instance name.
+     */
+    public constructor(
+        public readonly dnssd?: ahfc.ServiceDiscoveryDNSSDConfiguration,
+        public readonly databasePath = Settings.resolveAppDataPath(),
+        public readonly endpoint?: string,
+        public readonly port = 8080,
+        public readonly instanceName = crypto.randomBytes(4).toString("hex"),
+    ) { }
+
+    /**
+     * Loads settings object from JSON file at given path.
+     * 
+     * @param path Path to existing or non-existing settings JSON file.
+     * @return Settings object.
+     */
+    public static fromFileAt(path: string): Settings {
+        if (!fs.existsSync(path)) {
+            return new Settings();
+        }
+        const file = JSON.parse(fs.readFileSync(path, "utf8"));
+        return new Settings(
+            file.dnssd,
+            file.databasePath,
+            file.endpoint,
+            file.port,
+            file.instanceName
+        );
+    }
+
+    /**
+     * Resolves a file-system path to a suitable folder for storing application
+     * settings. Also creates the folder if it does not exist.
+     * 
+     * @return Path to application data folder.
+     */
+    public static resolveAppDataPath(): string {
+        if (this.appDataPath) {
+            return this.appDataPath;
+        }
+        let path = process.env.APPDATA;
+        if (!path) {
+            if (process.platform === "darwin") {
+                path = process.env.HOME + "/Library/Preferences";
+            } else {
+                path = process.env.HOME + "/.local";
+                mkdirIfNotExists(path);
+                path += "/share";
+                mkdirIfNotExists(path);
+            }
+        }
+        path += ("/ahfc-configuration-rest-ws");
+        mkdirIfNotExists(path);
+        return this.appDataPath = path;
+
+        function mkdirIfNotExists(path: string) {
+            if (!fs.existsSync(path)) {
+                fs.mkdirSync(path);
+            }
+        }
+    }
+}
--- source/main/main.ts
+++ source/main/main.ts
@@ -1,50 +1,140 @@
 import * as ahfc from "ahfc-client";
+import { ConfigurationSystem } from "./ConfigurationSystem";
+import * as db from "./db";
+import * as http from "./http";
+import * as process from "process";
+import { Settings } from "./Settings";
 
-let serviceDiscovery = new ahfc.ServiceDiscoveryDNSSD({
-    hostnames: ["arrowhead.eu"], // Name server hostname.
-    nameServers: ["0.0.0.0"], // IP address of name server.
-    transactionKey: {
-        name: undefined, // TSIG key name.
-        secret: undefined // TSIG key.
-    },
-});
+let serviceDiscovery;
+let serviceName;
+const serviceType = "_ahf-Configuration._http._tcp";
 
-serviceDiscovery
-    .publish({
-        serviceType: "_ahf-Configuration._http._tcp",
-        serviceName: "MyInstance",
-        endpoint: "example.com",
-        port: 80,
-        metadata: { crazy: "yes", encode: "xml" },
-    })
-    .then(_ => listAllServices())
-    .then(_ => serviceDiscovery.unpublish({
-        serviceType: "_ahf-Configuration._http._tcp",
-        serviceName: "MyInstance",
-    }))
-    .then(_ => listAllServices())
-    .catch(e => {
-        console.log(e);
+// Application start routine.
+function start() {
+    const appDataPath = Settings.resolveAppDataPath();
+    const config = Settings.fromFileAt(appDataPath + "/config.json");
+
+    serviceDiscovery = new ahfc.ServiceDiscoveryDNSSD(config.dnssd);
+    serviceName = config.instanceName;
+
+    serviceDiscovery.publish({
+        serviceType,
+        serviceName,
+        endpoint: config.endpoint,
+        port: config.port,
+    }).then(() => {
+        const directory = new db.DirectoryLMDB(config.databasePath);
+        new http.Server()
+            // Document handlers.
+            .handle({
+                method: http.Method.DELETE,
+                path: "/documents",
+                handler: (parameters, headers) => Promise.resolve({
+                    code: http.Code.NotImplemented,
+                    reason: "Not implemented",
+                }),
+            })
+            .handle({
+                method: http.Method.GET,
+                path: "/documents",
+                handler: (parameters, headers) => Promise.resolve({
+                    code: http.Code.NotImplemented,
+                    reason: "Not implemented",
+                }),
+            })
+            .handle({
+                method: http.Method.POST,
+                path: "/documents",
+                handler: (parameters, headers) => Promise.resolve({
+                    code: http.Code.NotImplemented,
+                    reason: "Not implemented",
+                }),
+            })
+
+            // Rule handlers.
+            .handle({
+                method: http.Method.DELETE,
+                path: "/rules",
+                handler: (parameters, headers) => Promise.resolve({
+                    code: http.Code.NotImplemented,
+                    reason: "Not implemented",
+                }),
+            })
+            .handle({
+                method: http.Method.GET,
+                path: "/rules",
+                handler: (parameters, headers) => Promise.resolve({
+                    code: http.Code.NotImplemented,
+                    reason: "Not implemented",
+                }),
+            })
+            .handle({
+                method: http.Method.POST,
+                path: "/rules",
+                handler: (parameters, headers) => Promise.resolve({
+                    code: http.Code.NotImplemented,
+                    reason: "Not implemented",
+                }),
+            })
+
+            // Template handlers.
+            .handle({
+                method: http.Method.DELETE,
+                path: "/templates",
+                handler: (parameters, headers) => Promise.resolve({
+                    code: http.Code.NotImplemented,
+                    reason: "Not implemented",
+                }),
+            })
+            .handle({
+                method: http.Method.GET,
+                path: "/templates",
+                handler: (parameters, headers) => Promise.resolve({
+                    code: http.Code.NotImplemented,
+                    reason: "Not implemented",
+                }),
+            })
+            .handle({
+                method: http.Method.POST,
+                path: "/templates",
+                handler: (parameters, headers) => Promise.resolve({
+                    code: http.Code.NotImplemented,
+                    reason: "Not implemented",
+                }),
+            })
+
+            .listen(config.port);
+    }, error => {
+        console.log("Failed to setup Arrowhead Configuration System.");
+        console.log("Cause:\n%s", error);
+        process.exit(1);
     });
+}
 
-function listAllServices(): Promise<void> {
-    return serviceDiscovery.lookupTypes()
-        .then(types => {
-            types.forEach(type => {
-                serviceDiscovery.lookupIdentifiers(type)
-                    .then(identifiers => identifiers.forEach(identifier => {
-                        serviceDiscovery.lookupRecord(identifier)
-                            .then(record => console.log("" + record))
-                            .catch(error => {
-                                console.log(error);
-                            });
-                    }))
-                    .catch(error => {
-                        console.log(error);
-                    });
-            });
-        })
-        .catch(error => {
-            console.log(error);
-        });
+// Application exit routine.
+function exit() {
+    serviceDiscovery.unpublish({ serviceType, serviceName }).then(() => {
+        process.exit(0);
+    }, error => {
+        console.log(error);
+        process.exit(2);
+    });
+}
+
+// Bootstrapping routine.
+{
+    let didExit = false;
+    const onExit = () => {
+        if (didExit) {
+            return;
+        }
+        didExit = true;
+        exit();
+    }
+
+    process.on("SIGINT", onExit);
+    process.on("SIGTERM", onExit);
+    process.on("SIGHUP", onExit);
+
+    start();
 }
